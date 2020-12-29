eda33911392dcb5b7fa063ab61d6c259b050dab4
Victoria Skeggs <vske511@aucklanduni.ac.nz>
2020-12-29 15:06:16 +1300

    Adds class to represent a chess move

---
+++ src/main/java/model/Colour.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Represents the colour of a chess team
+ */
+public enum Colour {
+    BLACK, WHITE;
+}
---
+++ src/main/java/model/PieceType.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Represents a type of chess piece
+ */
+public enum PieceType {
+    PAWN, CASTLE, KNIGHT, BISHOP, QUEEN, KING
+}
